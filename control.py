1933c160f5216d94fa0e8e3b1ce5366c9bcacd15
BOIIIIIIZ <matocham254@gmail.com>
2019-08-15 15:27:08 +0200

    RPi adjustments

--- control.py
+++ control.py
@@ -5,12 +5,14 @@ import datetime
 from mqtt_connect import send_message
 from air_module import air_sensor
 from iwlistparse import scan_wifi
-from trilateration import trilat
+from Trilateration import trilat
 
 def get_routers_positions():
-	dict = {"57.052359N,9.910457E":-44, "57.050374N,9.912322E":-45, "57.051868N,9.912837E":-54}
+	return {"57.052359N,9.910457E":-44, "57.050374N,9.912322E":-45, "57.051868N,9.912837E":-54}
 
 delay = 10
+
+print(get_routers_positions())
 
 while True:
 	dict = air_sensor()
