20dcdc1b451858c8ec07ee91e7eaafc1599c8897
miroslavmatocha <matocham254@gmail.com>
2019-08-15 15:08:23 +0200

    Trilateration implemented into control

--- control.py
+++ control.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
-
+import time
 import json
 import datetime
 from mqtt_connect import send_message
 from air_module import air_sensor
 from iwlistparse import scan_wifi
+from trilateration import trilat
 
-dict = air_sensor()
-dict['stations'] = scan_wifi()
-dict['datetime'] = unicode(datetime.datetime.now())
+def get_routers_positions():
+	dict = {"57.052359N,9.910457E":-44, "57.050374N,9.912322E":-45, "57.051868N,9.912837E":-54}
 
-send_message(json.dumps(dict))
+delay = 10
+
+while True:
+	dict = air_sensor()
+	dict['stations'] = scan_wifi()
+	dict['datetime'] = unicode(datetime.datetime.now())
+	dict['coords'] = trilat(get_routers_positions())
+
+	send_message(json.dumps(dict))
+	print("Message sent ... ")
+	time.sleep(10)
+
