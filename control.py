58356f9dff347696cf019dde504d05bb82cc1ee5
miroslavmatocha <matocham254@gmail.com>
2019-08-15 10:43:02 +0200

    updated control

--- control.py
+++ control.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import json
+import datetime
 from mqtt_connect import send_message
 from air_module import air_sensor
 from iwlistparse import scan_wifi
 
-send_message(air_sensor + iwlistparse)
+dict = air_sensor()
+dict['stations'] = scan_wifi()
+dict['datetime'] = datetime.datetime.now()
+
+send_message(json.dump(dict))
