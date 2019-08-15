efe2de5e69fd775d74f3d4991e9d063c87dcb720
miroslavmatocha <matocham254@gmail.com>
2019-08-15 16:39:18 +0200

    RGB module implemented into control

--- control.py
+++ control.py
@@ -2,25 +2,41 @@
 import time
 import json
 import datetime
-from mqtt_connect import send_message
+import mqtt_connect
 from air_module import air_sensor
 from iwlistparse import scan_wifi
 from Trilateration import trilat
+from rgb_module import rgb_led
 
 def get_routers_positions():
 	return {"57.052359N,9.910457E":-44, "57.050374N,9.912322E":-45, "57.051868N,9.912837E":-54}
 
-delay = 10
+def on_message(client, userdata, message):
+	if message.payload == "1":
+		rgb_led(255, 0, 0)
+	else if message.payload == "-1":
+		rgb_led(0, 0, 255)
+	else message.payload:
+		rgb_led(0, 255, 0)
 
-print(get_routers_positions())
+delay = 5
 
-while True:
-	dict = air_sensor()
-	dict['stations'] = scan_wifi()
-	dict['datetime'] = unicode(datetime.datetime.now())
-	dict['coords'] = trilat(get_routers_positions())
 
-	send_message(json.dumps(dict))
-	print("Message sent ... ")
-	time.sleep(10)
+mqtt_connect.init(on_message)
+print("Starting connection")
+time.sleep(1)
+
+try:
+	while True:
+		dict = air_sensor()
+		dict['stations'] = scan_wifi()
+		dict['datetime'] = unicode(datetime.datetime.now())
+		dict['coords'] = trilat(get_routers_positions())
+
+		mqtt_connect.send_message(json.dumps(dict))
+		print("Message sent")
+		time.sleep(10)
+except KeyboardInterrupt:
+	print("Closing connections...")
+	mqtt_connect.disconnect()
 
--- mqtt_connect.py
+++ mqtt_connect.py
@@ -9,13 +9,12 @@ def on_connect(client, userdata, flags, rc):
 
     if rc == 0:
         global Connected                #Use global variable
-        Connected = True                #Signal connection 
+        Connected = True
     else:
         print("Connection failed")
 
 
-
-def init():
+def init(on_message):
     global Connected
     Connected = False
     broker_address = "farmer.cloudmqtt.com"
@@ -26,21 +25,23 @@ def init():
     global Client
     Client = mqttClient.Client("RaspberryPi")               #create new instance
     Client.username_pw_set(user, password=password)    #set username and password 
-    Client.on_connect= on_connect                      #attach function to callback
+    Client.on_connect=on_connect                      #attach function to callback
     Client.connect(broker_address, port=port)          #connect to broker
+    Client.subscribe("rpi")
+    Client.on_message = on_message
     Client.loop_start()        #start the loop
 
-    
+def disconnect():
+    global Client
+    Client.disconnect()
+    Client.loop_stop()
 
 def send_message(msg):
     global Connected
-    Connected = False
 
-    while Connected != True:
-        init()
-        time.sleep(0.1)
+    if Connected == False:
+        return False
 
     global Client
     Client.publish("data", msg)
-    Client.disconnect()
-    Client.loop_stop()
+    return True
